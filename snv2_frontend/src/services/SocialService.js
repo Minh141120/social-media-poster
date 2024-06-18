3da214193d0c88cf1a0b944282bdfcc8d21b1827
Kuuhakuu <kuuhakuu@Kuuhakuus-MacBook-Pro.local>
2024-06-18 22:36:04 +0700

    change port for socialservice from 3003 to 3006

--- snv2_frontend/src/services/SocialService.js
+++ snv2_frontend/src/services/SocialService.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 // const baseUrl = 'http://localhost:3001/api/social' //! test json server
-const baseUrl = 'http://localhost:3003/api/social'
+const baseUrl = 'http://localhost:3006/api/social'
 
 const upload = async info => {
     const res = await axios.post(`${baseUrl}/upload`,info)
